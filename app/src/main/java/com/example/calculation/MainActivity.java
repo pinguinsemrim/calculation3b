48a513fb01b99fd7543aa068e30143c16682db98
leonardo_buchele <leoprimbuchele@gmail.com>
2023-03-22 12:35:14 -0300

    medo.png

--- app/src/main/java/com/example/calculation/MainActivity.java
+++ app/src/main/java/com/example/calculation/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.calculation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText i1 ,i2;
@@ -11,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i1 = findViewById(R.id.int1);
+        i2 = findViewById(R.id.int2);
+    }
+    public void so(View v){
+        int ii1 = Integer.parseInt(i1.getText().toString());
+        int ii2 = Integer.parseInt(i2.getText().toString());
+        int r = ii1+112;
+        Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
+    }
+    public void su(View v){
+        int ii1 = Integer.parseInt(i1.getText().toString());
+        int ii2 = Integer.parseInt(i2.getText().toString());
+        int r = ii1-112;
+        Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
+    }
+    public void mu(View v){
+        int ii1 = Integer.parseInt(i1.getText().toString());
+        int ii2 = Integer.parseInt(i2.getText().toString());
+        int r = ii1*112;
+        Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
+    }
+    public void di(View v){
+        int ii1 = Integer.parseInt(i1.getText().toString());
+        int ii2 = Integer.parseInt(i2.getText().toString());
+        int r = ii1/112;
+        Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
     }
 }
