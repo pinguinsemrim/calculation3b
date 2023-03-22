a01c440f32cdf067df67bcb52b919adcf280d731
leonardo_buchele <leoprimbuchele@gmail.com>
2023-03-22 12:13:37 -0300

    xml de base faz o resto misera

--- app/src/main/java/com/example/calculation/MainActivity.java
+++ app/src/main/java/com/example/calculation/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.calculation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText i1 ,i2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
