ce3d4343f87d96b3a5d1930d630b41ea938b134f
leonardo_buchele <leoprimbuchele@gmail.com>
2023-03-23 07:35:35 -0300

    medo.png

--- app/src/main/java/com/example/calculation/MainActivity.java
+++ app/src/main/java/com/example/calculation/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     EditText i1 ,i2;
     @Override
@@ -20,40 +22,33 @@ public class MainActivity extends AppCompatActivity {
         int ii1 = Integer.parseInt(i1.getText().toString());
         int ii2 = Integer.parseInt(i2.getText().toString());
         int r = ii1+112;
-        if(ii1==7||ii2==7){
-            Toast.makeText(this, "abobora", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
-        }
+        resp(ii1,ii2,r);
     }
     public void su(View v){
         int ii1 = Integer.parseInt(i1.getText().toString());
         int ii2 = Integer.parseInt(i2.getText().toString());
         int r = ii1-112;
-        if(ii1==45||ii2==44){
-            Toast.makeText(this, "erro bip bip bug", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
-        }
+        resp(ii1,ii2,r);
     }
     public void mu(View v){
         int ii1 = Integer.parseInt(i1.getText().toString());
         int ii2 = Integer.parseInt(i2.getText().toString());
         int r = ii1*112;
-        if(ii1==38||ii2==28){
-            Toast.makeText(this, "bug ocorreu", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
-        }
+        resp(ii1,ii2,r);
     }
     public void di(View v){
         int ii1 = Integer.parseInt(i1.getText().toString());
         int ii2 = Integer.parseInt(i2.getText().toString());
         int r = ii1/112;
-        if(ii1>=10||ii2>=10){
-            Toast.makeText(this, "erro erro", Toast.LENGTH_SHORT).show();
+       resp(ii1,ii2,r);
+    }
+    public void resp(int ii1, int ii2, int r){
+        Random rt = new Random();
+        if(ii1>=rt.nextInt(10) || ii2>=rt.nextInt(10)){
+            Toast.makeText(this, "erroz bip bip bug", Toast.LENGTH_SHORT).show();
         }else {
             Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
         }
     }
+
 }
